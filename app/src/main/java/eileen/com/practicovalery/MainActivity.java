3b48120c3eeac84c967ec706d91f066a76b8f1e8
MiTarea1 <estebanmolano963@gmail.com>
2019-03-07 19:38:24 -0500

    Primer commit

--- app/src/main/java/eileen/com/practicovalery/MainActivity.java
+++ app/src/main/java/eileen/com/practicovalery/MainActivity.java
@@ -1,13 +1,24 @@
 package eileen.com.practicovalery;
 
+import android.location.LocationManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.Marker;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_CODE = 11 ;
+    private GoogleMap mMap;
+    private LocationManager manager;
+    private Marker me;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
         setContentView(R.layout.activity_main);
     }
 }
